ef0f1a76a2f667a4ba3367443a85f2092379ef85
serviciosinformaticos132-dot <serviciosinformaticos132@gmail.com>
2026-01-01 23:13:07 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="PlaneaNEM IA",
+    description="API para generar planeaciones NEM",
+    version="0.1"
+)
+
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "PlaneaNEM IA activo",
+        "estado": "desarrollo"
+    }
 
